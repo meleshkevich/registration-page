bd870839441361312ddf4674eccac828bf673a99
Tomáš Kravčík <thomas.kravcik@gmail.com>
2022-05-27 09:52:18 +0200

    events list fetched

---
+++ src/js/Events.js
@@ -0,0 +1,16 @@
+const url_events = "https://test-api.codingbootcamp.cz/api/5c0eb69c/events"
+
+
+const fetchEventList = async() => {
+    const response = await fetch(url_events)
+    const data = await response.json()
+    console.log(data)
+    
+  }
+  
+
+
+
+export {
+    fetchEventList
+}  
---
+++ src/js/script.js
@@ -0,0 +1,5 @@
+import { fetchEventList } from "./Events";
+
+
+console.log("jkjkg")
+fetchEventList()
