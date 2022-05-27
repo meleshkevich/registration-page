2efa91a996fc50f9976b95e6b534afb26d45ffdc
Tomáš Kravčík <thomas.kravcik@gmail.com>
2022-05-27 12:20:06 +0200

    pull

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
+
 import { fetchEventList } from "./dataEventList";
-=======
-<<<<<<< HEAD
+
+
 // import { fetchEventList } from "./dataEventList";
 import FeatureEvent from "./FeatureEvent";
 import Event from "./Event";
@@ -14,23 +14,20 @@ const fetchEventList_featured = async () => {
   const data = await response.json();
   const newFeatured = new FeatureEvent(data[0].name);
 };
-=======
->>>>>>> main
->>>>>>> 77008728fd889b8b891481a0f6865044ea972e61
+
 
 fetchEventList_featured();
 
-<<<<<<< HEAD
 console.log("jkjkg") ///<<<<<<can be removed
 fetchEventList()
-=======
+
 const fetchEventList = async () => {
   const response = await fetch(url_events);
   const data = await response.json();
   data.forEach((el) => {
     const newEvent = new Event(el.name);
   });
->>>>>>> 77008728fd889b8b891481a0f6865044ea972e61
+
 
   const newEvent = new Event(data[0].name);
 };
